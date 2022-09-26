32f16846b92ad411fcae200b11bb9d4ef080ecdf
Joshua David Jennings <jennings.josh@gmail.com>
2022-09-25 23:06:12 -0500

    can post and delete

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -5,22 +5,20 @@ class CarsController < ApplicationController
     end
 
     post "/cars" do
-        car = Car.create(params)
-        car.to_json(include: :car)
-      end
+        car = Car.create({make:params[:make], model:params[:model], year:params[:year], color:params[:color], category:params[:category], motor:params[:motor], fuel_type:params[:fuel_type], transmission:params[:transmission], drive:params[:drive], year_bought:params[:year_bought], year_sold:params[:year_sold], driver_name:params[:driver_name]})
+        car.to_json
+    end
+  
+    patch "/cars/:id" do
+      car = Car.find(params[:id])
+      car.update({year_sold:params[:year_sold]})
+      car.to_json
+    end
     
-      #Update a car
-      patch "/cars/:id" do
-        car = Car.find(params[:id])
-    
-        car.update({year_sold:params[:year_sold]})
-        car.to_json(include: :car)
-      end
-      
-      #Delete a car
-      delete "/cars/:id" do
-        car = Car.find(params[:id])
-        car.destroy
-        {message: 'car deleted'}.to_json
-      end
+    #Delete a car
+    delete "/cars/:id" do
+      car = Car.find(params[:id])
+      car.destroy
+      {message: 'car deleted'}.to_json
+    end
 end
